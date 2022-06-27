d5684b20254b8526b546c31d6ad374af216ac5ea
aam <aam@MAA-OIO>
2022-06-27 10:52:02 +0900

    Проблема совместимисти типизации oracle mssql

--- ConsoleAppGetway/AppCode/Gateway.cs
+++ ConsoleAppGetway/AppCode/Gateway.cs
@@ -1,9 +1,12 @@
-﻿using ConsoleAppGateway.Models;
+﻿using ConsoleAppGateway.Logger;
+using ConsoleAppGateway.Models;
 using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Linq;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 
 namespace ConsoleAppGateway
@@ -13,13 +16,13 @@ namespace ConsoleAppGateway
         public string DbFrom { get => _dbFrom; set => _dbFrom = value; }
         public string DbTo { get => _dbTo; set => _dbTo = value; }
         public List<Table> Tablesl { get => _tablesl; set => _tablesl = value; }
-
-        public Gateway(string dbfrom, string dbto, List<Table> tables)
+        public int CountCommit { get => _countCommit; set => _countCommit = value; }
+        public Gateway(string dbfrom, string dbto, List<Table> tables, int countCommit = 5000)
         {
-            _dbFrom = dbfrom;
+            DbFrom = dbfrom;
             DbTo = dbto;
             Tablesl = tables;
-
+            CountCommit = countCommit;
         }
 
         public Gateway()
@@ -38,10 +41,11 @@ namespace ConsoleAppGateway
             catch (Exception ex)
             {
                 Stop();
+                _logger .AddLog($"Ошибка при старте: {ex.Message}", EventLogEntryType.Error);
                 throw new Exception($"Ошибка при старте: {ex.Message}");
             }
 
-            foreach(var table in _tablesl)
+            foreach (var table in _tablesl)
             {
                 TakeThisFromTheMssqlAndPutItDownOracle(table);
             }
@@ -56,34 +60,81 @@ namespace ConsoleAppGateway
             }
             catch (Exception ex)
             {
+                _logger .AddLog($"Ошибка при закрытии соеденения: {ex.Message}", EventLogEntryType.Error);
                 throw new Exception($"Ошибка при закрытии соеденения: {ex.Message}");
             }
         }
 
-        private void TakeThisFromTheMssqlAndPutItDownOracle (Table table)
+        private void TakeThisFromTheMssqlAndPutItDownOracle(Table table)
         {
             string listColNameFrom = "";
             table.ListColName.ForEach(x => listColNameFrom += $"{x.NameFrom},");
-            listColNameFrom = listColNameFrom.Remove(listColNameFrom.Length - 1, 1);
+            listColNameFrom = listColNameFrom.TrimEnd(',');
 
             string listColNameTo = "";
             table.ListColName.ForEach(x => listColNameTo += $"{x.NameTo},");
-            listColNameTo = listColNameTo.Remove(listColNameTo.Length - 1, 1);
+            listColNameTo = listColNameTo.TrimEnd(',');
 
             var sqlMs = $"select {listColNameFrom} from {table.From};";
             var cmdMs = new SqlCommand(sqlMs, _sqlConnection);
-            cmdMs.ExecuteNonQuery();
+            try
+            {
+                var reader = cmdMs.ExecuteReader();
 
-            //var sqlOra = $"insert into :tabName (:listColNameTo) values (:values)";
-            //var cmdOra = new OracleCommand(sqlOra, _oracleConnection);
-            //cmdOra.Parameters.Add("tabName", table.To);
-            //cmdOra.Parameters.Add("listColName", listColNameTo);
-            //cmdOra.Parameters.Add("values", listColName);
+                List<OracleCommand> listSqlOra = new List<OracleCommand>();
+
+                while (reader.Read())
+                {
+                    string values = "";
+                    for (var i = 0; i < reader.FieldCount; i++)
+                    {
+                        values += $"'{reader.GetValue(i)}',";
+                    }
+                    values = values.TrimEnd(',');
+                    listSqlOra.Add(new OracleCommand($"insert into {table.To} ({listColNameTo}) values ({values})", _oracleConnection));
+
+                    if (listSqlOra.Count == _countCommit)
+                    {
+                        InsertOracle(listSqlOra);
+                    }
+                }
+                if (listSqlOra.Count < _countCommit)
+                {
+                    InsertOracle(listSqlOra);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.AddLog($"Ошибка при получении данных: {ex.Message}", EventLogEntryType.Error);
+                throw new Exception($"Ошибка при получении данных: {ex.Message}");
+            }
+
         }
 
         private void TakeThisFromTheOracleAndPutItDownMssql(Table table)
         {
-          
+
+        }
+
+
+        private void InsertOracle(List<OracleCommand> listSqlOra)
+        {
+            OracleTransaction Tran = _oracleConnection.BeginTransaction(IsolationLevel.ReadCommitted);
+            try
+            {
+                foreach (var cmdOra in listSqlOra)
+                { 
+                    cmdOra.ExecuteNonQuery();
+                }
+                Tran.Commit();
+            }
+            catch (Exception ex)
+            {
+                Tran.Rollback();
+                _logger .AddLog($"Ошибка при вставки данных: {ex.Message}", EventLogEntryType.Error);
+                throw new Exception($"Ошибка при вставки данных: {ex.Message}");
+            }
         }
 
         private OracleConnection _oracleConnection;
@@ -91,5 +142,7 @@ namespace ConsoleAppGateway
         private string _dbFrom;
         private string _dbTo;
         private List<Table> _tablesl;
+        private int _countCommit;
+        private static readonly ILogger _logger  = new LoggerEvent("Gatway");
     }
 }
